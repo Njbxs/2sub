40a95a1aab463f535761012760308be85f8f981d
Njbxs <96908872+Njbxs@users.noreply.github.com>
2022-08-19 00:44:02 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -56,7 +56,7 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode("html")
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by Lord Ouraa\nhttps://t.me/Owaitingforyou")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by 𝘾 𝘼 𝙎 𝙏 𝙇 𝙀\nhttps://t.me/Candurate")
         self.username = usr_bot_me.username
 
     async def stop(self, *args):
